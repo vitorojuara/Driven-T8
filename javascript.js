77e88f8115a715e70c4785f35bfc3242126642e3
Vitor Araújo <vitor542ojuara@gmail.com>
2022-08-15 10:46:46 -0300

    Teste da quebra de linha

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 let selecoes = 0;
+let comidaWpp = '';
+let bebidaWpp = '';
+let sobremesaWpp = '';
+let totalWpp = '';
 
 function selectedFood(food) {
     const botao = document.querySelector('.menu-comidas .selected')
@@ -51,4 +55,26 @@ function selected() {
         elemento.classList.add('active')
         elemento.innerHTML = finishMSG;
     }
+}
+
+
+function wpp() {
+    const comidaWpp = document.querySelector('.menu-comidas .selected h2').innerHTML + ' ' +
+    document.querySelector('.menu-comidas .selected h3').innerHTML;
+    
+    const bebidaWpp = document.querySelector('.menu-bebidas .selected h2').innerHTML + ' ' +
+    document.querySelector('.menu-bebidas .selected h3').innerHTML;
+
+    const sobremesaWpp = document.querySelector('.menu-sobremesas .selected h2').innerHTML + ' ' +
+    document.querySelector('.menu-sobremesas .selected h3').innerHTML; 
+
+    const msgWpp = `Olá, gostaria de fazer o pedido: \n
+    - Prato: ${comidaWpp} \n
+    - Bebida: ${bebidaWpp} \n
+    - Sobremesa: ${sobremesaWpp} \n
+    Total : ${totalWpp}`
+
+    let encode = encodeURIComponent(msgWpp)   
+
+    window.open(`https://wa.me/5584999380219?text=${encode}`)
 }
